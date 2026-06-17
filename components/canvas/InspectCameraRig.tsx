74e44e698357bc1c32e67189efd1e5b8fd7fcdb4
Atta Navaid <a302@umbc.edu>
2026-06-16 23:45:40 -0400

    Refactor camera positioning in InspectCameraRig to utilize dynamic functions for improved flexibility and accuracy in model alignment.

--- components/canvas/InspectCameraRig.tsx
+++ components/canvas/InspectCameraRig.tsx
@@ -3,14 +3,18 @@
 import { useLayoutEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import * as THREE from "three";
-import { INSPECT_LAYOUT } from "@/lib/inspectLayout";
+import {
+  getInspectCameraPosition,
+  INSPECT_LAYOUT,
+  INSPECT_LOOK_AT,
+} from "@/lib/inspectLayout";
 
 export function InspectCameraRig() {
   const camera = useThree((state) => state.camera);
 
   useLayoutEffect(() => {
-    camera.position.set(...INSPECT_LAYOUT.cameraPosition);
-    camera.lookAt(new THREE.Vector3(...INSPECT_LAYOUT.lookAt));
+    camera.position.set(...getInspectCameraPosition());
+    camera.lookAt(new THREE.Vector3(...INSPECT_LOOK_AT));
     if (camera instanceof THREE.PerspectiveCamera) {
       camera.fov = INSPECT_LAYOUT.fov;
       camera.updateProjectionMatrix();
